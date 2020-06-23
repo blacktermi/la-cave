8a148ca8c8300454096a9e21342332de85cbb179
josephkakou <blacktermi@protonmail.com>
2020-06-23 20:12:40 +0000

    mase a jour 1.1 ajout page de payment (blockage sur la recuperation des images dans le panier)

--- main.js
+++ main.js
@@ -66,6 +66,7 @@ let products =[
 for (let i=0; i < carts.length; i++) {
     carts[i].addEventListener('click', ()=> {
         cartNumbers(products[i]);
+        totalcost(products[1])
     })
 }
 
@@ -114,9 +115,41 @@ function setItems(product) {
         cartItems = {
             [product.tag]: product
         }
-    }
-    
-    
+    }   
     localStorage.setItem("productsIncart", JSON.stringify (cartItems));
 }
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+
+    if(cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem('totalCost', cartCost +
+        product.price);
+    } else{
+        localStorage.setItem("totalCost", product.price);
+    }
+
+    
+}
+function displayCart () {
+    let cartItems = localStorage.getItem("productIncart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".product-container");
+    
+    
+    if(cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class ="product">
+                <ion-icon name="close-circle-outline"></ion-icon>
+                <img src="img/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            `
+        })
+    }
+}
+
 onloadCartNumbers();
